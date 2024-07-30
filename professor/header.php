8365751eb2a335432df0b52766cbc383157d8888
prince_ceddie <99298197+cedkiller@users.noreply.github.com>
2024-07-30 14:39:07 +0800

    4th Commit

---
+++ professor/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GRC Examination</title>
+    <link rel="stylesheet" href="../professor/css/style2.css">
+    <link rel="icon" href="../img/new3.png">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://kit.fontawesome.com/75fe70a6d6.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!-- header -->
+    <div class="header">
+        <div class="icon-container">
+            <i class="fa-solid fa-bars icon" id="bar-icon"></i>
+            <i class="fa-solid fa-x icon" id="x-icon" style="display: none;"></i>
+        </div>
+
+        <div class="logo-container">
+            <img src="../img/LOGO_ORIGINAL-removebg-preview.png" alt="" class="img">
+        </div>
+
+        <div class="dropdown">
+            <button class="dropbtn">Dropdown</button>
+            <div class="dropdown-content">
+                <a href="#">Admin Profile</a>
+                <a href="#">Logout</a>
+            </div>
+        </div>
+    </div>
